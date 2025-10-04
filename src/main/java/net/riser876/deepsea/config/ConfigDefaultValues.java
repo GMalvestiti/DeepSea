e6108965dc8092c2f62301ed6471bdc3bbbb50e6
GMalvestiti <gustavo.smalvestiti@gmail.com>
2025-10-04 18:19:59 -0300

    [fix] mixin plugin fix

--- src/main/java/net/riser876/deepsea/config/ConfigDefaultValues.java
+++
@@ -1,8 +0,0 @@
-package net.riser876.deepsea.config;
-
-public class ConfigDefaultValues {
-
-    public static final boolean ENABLED = true;
-    public static final int DEEP_SEA_TICK_INTERVAL = 100;
-    public static final float DEEP_SEA_BOAT_DAMAGE = 100.0f;
-}
