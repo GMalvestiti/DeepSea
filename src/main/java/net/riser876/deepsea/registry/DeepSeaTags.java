53aa42f65c81e7ba0e86c2b04fd3d6f3f9431bbd
Gustavo Malvestiti <101594283+GMalvestiti@users.noreply.github.com>
2025-12-28 02:58:48 -0300

    [feat] port minecraft 1.21.11 (#22)

--- src/main/java/net/riser876/deepsea/registry/DeepSeaTags.java
+++ src/main/java/net/riser876/deepsea/registry/DeepSeaTags.java
@@ -1,12 +1,12 @@
 package net.riser876.deepsea.registry;
 
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.biome.Biome;
 
 public class DeepSeaTags {
-    public static final TagKey<EntityType<?>> DEEP_SEA_BOAT = TagKey.create(Registries.ENTITY_TYPE, ResourceLocation.fromNamespaceAndPath("deepsea", "boat"));
-    public static final TagKey<Biome> DEEP_SEA_BIOME = TagKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath("deepsea", "ocean"));
+    public static final TagKey<EntityType<?>> DEEP_SEA_BOAT = TagKey.create(Registries.ENTITY_TYPE, Identifier.fromNamespaceAndPath("deepsea", "boat"));
+    public static final TagKey<Biome> DEEP_SEA_BIOME = TagKey.create(Registries.BIOME, Identifier.fromNamespaceAndPath("deepsea", "ocean"));
 }
